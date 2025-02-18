c4aae1d44fb6a0d5a3a2d3ab7951c9d51ae51eea
khaledram14 <khaledramdani213@gmail.com>
2025-02-18 04:51:39 +0100

    modification jwt dans le login , login et signup a la bd en ligne marche a 100%

--- twist/app/api/auth/login/route.ts
+++ twist/app/api/auth/login/route.ts
@@ -6,25 +6,48 @@ import jwt from "jsonwebtoken";
 export async function POST(req: Request) {
   const { email, password } = await req.json();
 
-  if (!email || !password) return NextResponse.json({ error: "Tous les champs sont requis" }, { status: 400 });
+  if (!email || !password)
+    return NextResponse.json(
+      { error: "Tous les champs sont requis" },
+      { status: 400 }
+    );
 
   try {
-    const [userResult] = await pool.query("SELECT * FROM users WHERE email = ?", [email]);
+    const [userResult] = await pool.query(
+      "SELECT * FROM users WHERE email = ?",
+      [email]
+    );
     console.log("emal");
-    if ((userResult as any[]).length === 0) return NextResponse.json({ error: "Email ou mot de passe incorrect" }, { status: 401 });
+    if ((userResult as any[]).length === 0)
+      return NextResponse.json(
+        { error: "Email ou mot de passe incorrect" },
+        { status: 401 }
+      );
 
     const user = (userResult as any[])[0];
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     console.log("bcrypt");
 
-    if (!isPasswordValid) return NextResponse.json({ error: "Mot de passe incorrect" }, { status: 401 });
+    if (!isPasswordValid)
+      return NextResponse.json(
+        { error: "Mot de passe incorrect" },
+        { status: 401 }
+      );
     console.log("NextResponse");
 
-    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET as string, { expiresIn: "24h" });
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      "MySuperSecretKey123!",
+      { expiresIn: "24h" }
+    );
+
     console.log("jwt");
 
-    return NextResponse.json({ message: "Connexion réussie", token }, { status: 200 });
+    return NextResponse.json(
+      { message: "Connexion réussie", token },
+      { status: 200 }
+    );
   } catch (error) {
     return NextResponse.json({ error: "Erreur serveur" }, { status: 500 });
   }
